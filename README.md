35a01aefac7e44184da48534435a79cee1d0bcbe
Eric Backhage <eric.backhage@gmail.com>
2025-08-18 08:28:55 +0200

    Create a db engine

--- README.md
+++ README.md
@@ -13,3 +13,8 @@ in the book.
 SQLAlchemy supports multiple different databases. I choose to use SQLite due to
 its simplicity, that Python has built in support for it and because it doesn't
 require a separate server to run.
+
+The SQLAlchemy library consists of two modules called Core and ORM. An
+application can use Core only, or take advantage of the ORM module. The approach
+taken here is the combined approach where both the Core and the ORM functionality
+is being used.
--- db.py
+++ db.py
@@ -1,7 +1,15 @@
 import os
 
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 load_dotenv()
 
 print(f"Database URL: {os.environ['DATABASE_URL']}")
+
+# The engine manages connections to a database.
+# Some nice to know options are:
+#   echo = True, to have SQLAlchemy log every SQL statement
+#   pool_size=<N>, set a custom size for the connection pool (default 5)
+#   max_overflow=<N>, max number of connections that can be created during spikes (default 10)
+engine = create_engine(os.environ["DATABASE_URL"], echo=True)
