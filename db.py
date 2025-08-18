878bb4778b386c9329810838c34ef2e6c41f1965
Eric Backhage <eric.backhage@gmail.com>
2025-08-18 19:15:47 +0200

    Define naming conventions

--- db.py
+++ db.py
@@ -1,14 +1,24 @@
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import MetaData, create_engine
 from sqlalchemy.orm import DeclarativeBase
 
 
 # The parent class for all database model classes.
 # This holds settings that are common to all the tables.
 class Model(DeclarativeBase):
-    pass
+    metadata = MetaData(
+        # Explicitly define naming conventions to avoid problems with
+        # constraints getting unknown names assigned by the db.
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
 
 
 load_dotenv()
