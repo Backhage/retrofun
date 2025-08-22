a9c17b76a737d1d695640157c5a652d021bc93ff
Eric Backhage <eric.backhage@gmail.com>
2025-08-22 08:43:22 +0200

    Add session

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from sqlalchemy import MetaData, create_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 
 # The parent class for all database model classes.
@@ -31,3 +31,9 @@ print(f"Database URL: {os.environ['DATABASE_URL']}")
 #   pool_size=<N>, set a custom size for the connection pool (default 5)
 #   max_overflow=<N>, max number of connections that can be created during spikes (default 10)
 engine = create_engine(os.environ["DATABASE_URL"], echo=True)
+
+# The session maintains the list of new, read, modified, and deleted model instances
+# Changes are passed on to the database in the context of a transaction when the
+# session is flushed. When the session is committed the changes are permanently
+# written to the db.
+Session = sessionmaker(engine)
