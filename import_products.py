f96a07da0a5f07d0731c12dc286cb7cb6ce9a6ed
Eric Backhage <eric.backhage@gmail.com>
2025-08-25 08:15:24 +0200

    Add import of products from csv

---
+++ import_products.py
@@ -0,0 +1,23 @@
+# Imports products from a CSV file
+import csv
+
+from db import Model, Session, engine
+from models import Product
+
+
+def main():
+    Model.metadata.drop_all(engine)  # warning: this deletes all data in the db!
+    Model.metadata.create_all(engine)
+
+    with Session() as session:
+        with session.begin():
+            with open("products.csv") as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    row["year"] = int(row["year"])
+                    product = Product(**row)
+                    session.add(product)
+
+
+if __name__ == "__main__":
+    main()
