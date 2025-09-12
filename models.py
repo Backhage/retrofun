49ba83a1398630a83a336dee8a70b71bcdff4062
Eric Backhage <eric.backhage@gmail.com>
2025-09-12 08:50:22 +0200

    Added some comments in code

--- models.py
+++ models.py
@@ -13,9 +13,16 @@ class Product(Model):
         primary_key=True
     )  # Defaults to autoincrement starting from 1
     name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    # This defines a one-to-many relationship. Each manufacturer (the one) can
+    # have many products (the many). This relationship is defined on the "many"
+    # side of the model, in this case the Product.
     manufacturer_id: Mapped[int] = mapped_column(
         ForeignKey("manufacturers.id"), index=True
     )
+    # The `back_populates` relationship does not result in any information added
+    # to the database, it is a strictly SQLAlchemy construction which allows for
+    # SQLAlchemy to query the database when the `manufacturer` field is accessed
+    # in the code.
     manufacturer: Mapped["Manufacturer"] = relationship(back_populates="products")
     year: Mapped[int] = mapped_column(index=True)
     country: Mapped[Optional[str]] = mapped_column(String(32))
@@ -30,6 +37,10 @@ class Manufacturer(Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    # As with the Product model, the `back_populates` relationship does not
+    # result in any information added to the database. It only allows SQLAlchemy
+    # to query the database for products for a given manufacturer when the `products`
+    # field is accessed.
     products: Mapped[list["Product"]] = relationship(back_populates="manufacturer")
 
     def __repr__(self):
