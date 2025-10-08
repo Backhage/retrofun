2b23bae47d7189e6a635372a4291ee493ded886d
Eric Backhage <eric.backhage@gmail.com>
2025-10-08 08:45:26 +0200

    Added alembic migration support

--- import_products.py
+++ import_products.py
@@ -1,13 +1,19 @@
 # Imports products from a CSV file
 import csv
 
-from db import Model, Session, engine
-from models import Country, Manufacturer, Product
+from sqlalchemy import delete
+
+from db import Session
+from models import Country, Manufacturer, Product, ProductCountry
 
 
 def main():
-    Model.metadata.drop_all(engine)  # warning: this deletes all data in the db!
-    Model.metadata.create_all(engine)
+    with Session() as session:
+        with session.begin():
+            session.execute(delete(ProductCountry))
+            session.execute(delete(Product))
+            session.execute(delete(Manufacturer))
+            session.execute(delete(Country))
 
     with Session() as session:
         with session.begin():
