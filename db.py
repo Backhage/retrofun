0f089f1cae88d7b019d27b41e438820ceaecfe62
Eric Backhage <eric.backhage@gmail.com>
2026-01-19 18:25:02 +0100

    Added import scripts for articles and views

--- db.py
+++ db.py
@@ -30,7 +30,7 @@ print(f"Database URL: {os.environ['DATABASE_URL']}")
 #   echo = True, to have SQLAlchemy log every SQL statement
 #   pool_size=<N>, set a custom size for the connection pool (default 5)
 #   max_overflow=<N>, max number of connections that can be created during spikes (default 10)
-engine = create_engine(os.environ["DATABASE_URL"], echo=True)
+engine = create_engine(os.environ["DATABASE_URL"])
 
 # The session maintains the list of new, read, modified, and deleted model instances
 # Changes are passed on to the database in the context of a transaction when the
